4ffc51cad0cdffc8be6774394165caeda6bbadfe
Brian Youngblood <brian@brianyoungblood.com>
2015-12-11 18:10:16 -0600

    Update version to 3.6.1

--- Formula/gam.rb
+++ Formula/gam.rb
@@ -3,7 +3,7 @@ class Gam < Formula
   homepage "https://github.com/jay0lee/GAM"
   url "https://github.com/jay0lee/GAM/archive/v3.61.tar.gz"
   version "3.61"
-  sha256 "acd73486047bcd8d26bf164363d9d9d6e48acf99632e7ca658ebbe6c554e844b9"
+  sha256 "acd73486047bcd8d26bf164363d9d9d6e48acf99632e7ca658ebbe6c554e844b"
   head "https://github.com/jay0lee/GAM.git"
 
   def install
