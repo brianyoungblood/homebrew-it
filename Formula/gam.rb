ff25ac7c4dcb6020eca6503e63097caa7bce8b9d
Brian Youngblood <brian@brianyoungblood.com>
2017-08-30 16:10:06 -0500

    update to 4.30

--- Formula/gam.rb
+++ Formula/gam.rb
@@ -1,9 +1,9 @@
 
 class Gam < Formula
   homepage "https://github.com/jay0lee/GAM"
-  url "https://github.com/jay0lee/GAM/archive/v3.65.tar.gz"
-  version "3.65"
-  sha256 "02ee1a5f5fcff5cd65a73786221a3a35fab38c12675060f8d0a0331216ca141c"
+  url "https://github.com/jay0lee/GAM/archive/v4.30.tar.gz"
+  version "4.30"
+  sha256 "846b2902fe36019a9f4ebd4a911e780e103fb2d210971b8a4e407ddb6d8db4f6"
   head "https://github.com/jay0lee/GAM.git"
 
   def install
@@ -14,6 +14,6 @@ class Gam < Formula
   end
 
   test do
-    system "#{bin}/gam | grep 'GAM 3.65'"
+    system "#{bin}/gam | grep 'GAM 4.30'"
   end
 end
